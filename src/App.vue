47acf9bab7204c3d2fc65e8093351f4f2e852727
pvermeil <pvermeil@smag.tech>
2021-03-29 13:36:13 +0200

    form completed

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
+  <SignupForm/>
 </template>
 
 <script>
 
+import SignupForm from "./components/SignupForm.vue";
+
 export default {
   name: 'App',
-  components: {}
+  components: {SignupForm}
 }
 </script>
 
